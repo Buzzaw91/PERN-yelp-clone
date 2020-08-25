1b29515782a1323134a067a1a550bc7c955cd6a5
Buzzaw91 <aleksi.ylonen2@gmail.com>
2020-08-25 17:15:35 +0300

    refactor: remove unnecessary code

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,7 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT
-});
+const pool = new Pool();
 
 
 
