c03bc04bb853ec9c2db15735fbe5e8383db17f07
Buzzaw91 <aleksi.ylonen2@gmail.com>
2020-08-26 13:59:22 +0300

    feat: init contextApi

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,28 @@ import React, { Suspense } from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './routes/Home';
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
 
 const RestaurantDetail = React.lazy(() => import('./routes/RestaurantDetail'));
 const UpdatePage = React.lazy(() => import('./routes/UpdatePage'));
 
 const App = () => {
-  return <div className="container">
-    <Suspense fallback={<div></div>}>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/restaurants/:id/update" component={UpdatePage} />
-          <Route exact path="/restaurants/:id" component={RestaurantDetail} />
-        </Switch>
-      </Router>
-    </Suspense>
-  </div>
+
+  return (
+    <RestaurantsContextProvider>
+      <div className="container">
+        <Suspense fallback={<div></div>}>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/restaurants/:id/update" component={UpdatePage} />
+              <Route exact path="/restaurants/:id" component={RestaurantDetail} />
+            </Switch>
+          </Router>
+        </Suspense>
+      </div>
+    </RestaurantsContextProvider>
+  )
 }
 
 export default App;
