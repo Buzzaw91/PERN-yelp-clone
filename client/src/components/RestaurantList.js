1623fb3e4142e4799dd3aeedd22d719e7bc3f210
Buzzaw91 <aleksi.ylonen2@gmail.com>
2020-08-26 14:47:29 +0300

    feat: rendering the data

--- client/src/components/RestaurantList.js
+++ client/src/components/RestaurantList.js
@@ -33,7 +33,19 @@ const RestaurantList = (props) => {
           </tr>
         </thead>
         <tbody>
-          <tr>
+          {restaurants && restaurants.map(restaurant => {
+            return (
+              <tr key={restaurant.id}>
+                <td>{restaurant.name}</td>
+                <td>{restaurant.location}</td>
+                <td>{"$".repeat(restaurant.price_range)}</td>
+                <td>Reviews</td>
+                <td><button className="btn btn-warning">Update</button></td>
+                <td><button className="btn btn-danger">Delete</button></td>
+              </tr>
+            );
+          })}
+          {/* <tr>
             <td>McDonalds</td>
             <td>New York</td>
             <td>$$</td>
@@ -56,7 +68,7 @@ const RestaurantList = (props) => {
             <td>
               <button className="btn btn-danger">Delete</button>
             </td>
-          </tr>
+          </tr> */}
         </tbody>
       </table>
     </div>
