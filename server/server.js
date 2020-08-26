2384ff9157da8b3948677f6bb26ac246f1984c33
Buzzaw91 <aleksi.ylonen2@gmail.com>
2020-08-26 14:26:03 +0300

    feat: cors & fetch data from server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes/router');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
