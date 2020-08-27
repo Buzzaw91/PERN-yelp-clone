b1b720210fa9cd4392382079ea1d46c9281dccfc
Buzzaw91 <aleksi.ylonen2@gmail.com>
2020-08-27 20:22:01 +0300

    feat: update SQL queries to join reviews info

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,12 +2,16 @@ const db = require('../db');
 
 module.exports.getRestaurants = async (req, res) => {
   try {
-    const results = await db.query("SELECT * FROM restaurants;");
+    // const results = await db.query("SELECT * FROM restaurants;");
+    const restaurantRatingsData = await db.query(`
+    SELECT * FROM restaurants LEFT JOIN (SELECT restaurant_id, COUNT(*), trunc(AVG(rating),1) AS average_rating FROM reviews
+    GROUP BY restaurant_id) reviews ON restaurants.id = reviews.restaurant_id;
+    `);
     res.status(200).json({
       status: 'success',
-      results: results.rows.length,
+      results: restaurantRatingsData.rows.length,
       data: {
-        restaurant: results.rows
+        restaurant: restaurantRatingsData.rows
       }
     });
   }
@@ -19,7 +23,10 @@ module.exports.getRestaurants = async (req, res) => {
 module.exports.getOneRestaurant = async (req, res) => {
   const param = req.params.id;
   try {
-    const restaurant = await db.query(`SELECT * FROM restaurants WHERE id = $1;`, [param]);
+    const restaurant = await db.query(`
+    SELECT * FROM restaurants LEFT JOIN (SELECT restaurant_id, COUNT(*), trunc(AVG(rating),1) AS average_rating FROM reviews
+    GROUP BY restaurant_id) reviews ON restaurants.id = reviews.restaurant_id WHERE id = $1;
+    `, [param]);
 
     const reviews = await db.query(`SELECT * FROM reviews WHERE restaurant_id = $1;`, [param]);
 
